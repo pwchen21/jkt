11adc577ebbefd669b9e498d3d0ae3bde965c5de
pwchen21 <pwchen21@gmail.com>
2022-01-19 04:23:55 +0800

    add test case 5

--- test_param.py
+++ test_param.py
@@ -3,8 +3,8 @@ import math
 
 @pytest.mark.parametrize(
     "base, expo, expec",
-    [(2,2,4), (2,3,8), (1,9,1), (0,9,0)],
-    ids=["case1", "case2", "case3", "case4"]
+    [(2,2,4), (2,3,8), (1,9,1), (0,9,0), (3,3,27)],
+    ids=["case1", "case2", "case3", "case4", "case5"]
     )
 
 def test_pow(base, expo, expec):
